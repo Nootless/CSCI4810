096a0f264d39925b2999086f57966adb1df98b9a
Justin Z <justin.zhang0403@gmail.com>
2022-09-10 16:08:17 -0400

    Added comments, fixed all bugs

--- python/p1-p2/main.py
+++ python/p1-p2/main.py
@@ -2,11 +2,16 @@ from PIL import Image
 from utils import basic_draw, brz_draw
 import random
 
+### README 
+# To run this, you must have PIL package installed
+# utils.py needs to be in the same directory
+# math, random, and time come native with Python
 def test_basic(image):
     
     # Horizontal Line Test Case
     basic_draw(0, 150, 100, 150, image)
     image.show()
+    
     # Vertical Line Test Case
     basic_draw(100, 0, 100, 150, image)
     image.show()
@@ -131,46 +136,79 @@ def test_brz(image):
     # Slope less than 1
     brz_draw(150, 25, 100, 50, image)
     image.show()
-
-def user_input_test(lines,image):
-    time_total = 0
+    
+# this function is used as the main program
+# Bresenham algorithm picture follows basic algorithm picture
+def user_input_test(lines,image,image2):
+    time_total_basic = 0
+    time_total_brz = 0
     for x in range(lines):
         # random number generation
         x0 = random.randrange(0,200,1)
         x1 = random.randrange(0,200,1)
         y0 = random.randrange(0,200,1)
         y1 = random.randrange(0,200,1)
-        # time_total += basic_draw(x0, y0, x1, y1, image)
-        time_total += brz_draw(x0, y0, x1, y1, image)    
-    print(f'Time total: {time_total}')
+        time_total_basic += basic_draw(x0, y0, x1, y1, image)
+        time_total_brz += brz_draw(x0, y0, x1, y1, image2)    
+    print(f'Time total_basic: {time_total_basic}\nTime total brz: {time_total_brz}')
     image.show()
+    image2.show()
     
+# this function is to test the time of longer lines    
 def test_line_count_long(lines, image):
     time_total_basic = 0
     time_total_brz = 0
     for x in range(lines):
-        time_total_basic += basic_draw(0,0,25,25,image)
-        time_total_brz += brz_draw(0,0,25,25,image)
+        time_total_basic += basic_draw(0,0,100,150,image)
+        time_total_brz += brz_draw(0,0,100,150,image)
     print(f'Time total basic: {time_total_basic}\nTime total brz: {time_total_brz}')
-        
+
+# this function is to test the time of shorter lines
 def test_line_count_short(lines, image):
     time_total_basic = 0
     time_total_brz = 0
     for x in range(lines):
-        time_total_basic += basic_draw(0,0,100,100,image)
-        time_total_brz += brz_draw(0,0,100,100,image)
+        time_total_basic += basic_draw(0,0,25,20,image)
+        time_total_brz += brz_draw(0,0,25,20,image)
     print(f'Time total basic: {time_total_basic}\nTime total brz: {time_total_brz}')
+
+
+# this function is to test for slope differences using short lines
+def test_slopes(lines, image):
+    time_total_basic_greater = 0
+    time_total_basic_lesser = 0
+    time_total_brz_greater = 0
+    time_total_brz_lesser = 0
+    
+    for x in range(lines):
+        # >1 Slope
+        time_total_basic_greater += basic_draw(0,0,20,25,image)
+        time_total_brz_greater += brz_draw(0,0,20,25,image)
+        
+        # <1 Slope
+        time_total_basic_lesser += basic_draw(0,0,25,20,image)
+        time_total_brz_lesser += brz_draw(0,0,25,20,image)
+    print(f'Time total basic <1: {time_total_basic_lesser}\
+            \nTime total basic >1: {time_total_basic_greater}\
+            \nTime total brz < 1: {time_total_brz_lesser}\
+            \nTime total brz > 1: {time_total_brz_greater}\n')
+
     
 if __name__ == '__main__':
     lines = int(input('How Many Lines? (Integers only):'))
     image = Image.new('L',(200,200))
+    image2 = Image.new('L',(200,200))  
+    # user input
+    user_input_test(lines,image,image2)
          
-    # run for test
+    # run for test - used to display functionality
     # test_basic(image)
     # test_brz(image)
-    # test_line_count_long(lines,image)
-    # test_line_count_short(lines,image)
     
-    # user input
-    user_input_test(lines,image)
+    # repitition tests - time comparisons
+    # for x in [1,10,100,1000,10000,100000]:
+    #     print(f'{x} Lines')
+    #     test_line_count_long(x,image)
+    #     test_line_count_short(x,image)
+    #     test_slopes(x,image)
     
--- python/p1-p2/utils.py
+++ python/p1-p2/utils.py
@@ -1,5 +1,4 @@
 from math import trunc
-from tkinter import Image
 import time
 
 # basic draw drawing algorithm from in class notes
@@ -50,12 +49,11 @@ def basic_draw(x0, y0, x1, y1, image):
         m = del_y / del_x
         # timer start
         time_start = time.perf_counter()
-        
         # critical loop
         for i in range (del_x + 1):
-            x = x0 + i
-            y = m * i + y0
-            y = trunc(y)
+            x = x0 + i # shift over
+            y = m * i + y0 # calculate y point from slope and initial
+            y = trunc(y) # truncate to work on int point system
             image.putpixel((x,y), (255))
         
         # finish of critical loop, delta time calculated
@@ -91,23 +89,31 @@ def brz_draw(x0, y0, x1, y1, image):
     else:
         step_x = 1
     
-    # bit shift dy and dx
+    # bit shift dy and dx (multiply by 2)
     dy <<= 1;
     dx <<= 1;
+    
     if d_x == 0:
-        # changes orientation
+        # vertical line
+        # assigns the lower bound to y0
         if y1 < y0:
             y0 = y1
             d_y = -d_y
-        for y_range in range(d_y + 1):
+        for y_range in range(d_y + 1): 
             image.putpixel((x0, y0 + y_range), (255))
+            
     elif d_y == 0:
+        # horizontal line
+        # assigns lower bound to x0
         if x1 < x0:
             x0 = x1
             d_x = -d_x
         for x_range in range(d_x + 1):
             image.putpixel((x0 + x_range, y0), (255))
     elif d_y/d_x == 1:
+        # 45 degree line
+        # rearranges to assign lower bound to both y0 and x0
+        # to allow for inverted input
         if x1 < x0:
             y0 = y1
             x0 = x1
@@ -117,6 +123,9 @@ def brz_draw(x0, y0, x1, y1, image):
             image.putpixel((x0 + x_range, y0), (255))
             y0 += 1
     elif d_y/d_x == -1:
+        # 315 degree line
+        # rearranges to assign lower bound to both y0 and x0
+        # to allow for inverted input 
         if x1 < x0:
             y0 = y1
             x0 = x1
@@ -126,13 +135,19 @@ def brz_draw(x0, y0, x1, y1, image):
             image.putpixel((x0 + x_range, y0), (255))
             y0 -= 1     
     else:
+        # uses x as the iterator, y as the calculated point
         if dx > dy:
+            # this is the amount y has increased since last increment
+            # used to represent if y needs to increase or stay the same
             fraction = dy - (dx >> 1)
             
             # critical loop
             time_start = time.perf_counter()
             while x0 != x1:
                 image.putpixel((x0,y0),(255))
+                # if y needs to increase, adjust y by the step and
+                # decrement the fraction two times the difference in x0 and x1
+                # otherwise the y stays at its current value
                 x0 += step_x
                 if fraction >= 0:
                     y0 += step_y
@@ -140,13 +155,20 @@ def brz_draw(x0, y0, x1, y1, image):
                 fraction += dy
             time_end = time.perf_counter()
             timef = time_end - time_start
+            
         else:
+            # uses y as the iterator, x as the calculated point
+            # this is the amount x has increased since last increment
+            # used to represent if x needs to increase or stay the same
             fraction = dx - (dy >> 1)
             
             # critical loop
             time_start = time.perf_counter()
             while y0 != y1:
                 image.putpixel((x0,y0),(255))
+                # if x needs to increase, adjust x by the step and
+                # decrement two times the difference in y0 and y1
+                # otherwise the x stays at its current value
                 if fraction >= 0:
                     x0 += step_x
                     fraction -= dy
