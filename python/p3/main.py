a2bb37f4b01a5a7f2ab85c906322ea0b94006ccb
Justin <justin.zhang0403@gmail.com>
2022-09-26 22:34:03 -0400

    translate function and gui finished

--- python/p3/main.py
+++ python/p3/main.py
@@ -4,27 +4,59 @@ from PIL import Image
 import utils
 import numpy
 
+# global declares
+global lines_array
+global transforms 
+transforms = numpy.array([[1, 0, 0],[0, 1, 0], [0, 0, 1]])
 
 # create App
 # comes from https://www.youtube.com/watch?v=ELkaEpN29PU
 
 def import_file():
-    filename = filedialog.askopenfilename()
     global lines_array
-    lines_array = numpy.genfromtxt(f'{filename}',delimiter=',')
-    
-    # textbox update
-    text_box.delete('1.0','end')
-    text_box.insert(END,lines_array)
-    text_box_info.delete('1.0','end')
-    text_box_info.insert(END,f'Length:{len(lines_array)}')
-    print(len(lines_array))
+    global transforms 
+    filename = filedialog.askopenfilename()
+    # check for no input
+    if(len(filename) != 0):
+        lines_array = numpy.genfromtxt(f'{filename}',delimiter=',')
+        # textbox update
+        text_box.delete('1.0','end')
+        text_box.insert(END,lines_array)
+        text_box_info.delete('1.0','end')
+        text_box_info.insert(END,f'Length:{len(lines_array)}')
+
 
 def export_file():
     numpy.savetxt(f'{output_entry.get()}.csv',lines_array,delimiter=',')
-    print('export file')
+
+def basic_translate(original, t_x,t_y):
+    global lines_array
+    global transforms 
+    trans_matrix = numpy.array([[1, 0, 0],[0, 1, 0],[t_x,t_y, 1]])
+    trans_matrix = trans_matrix.astype(float)
+    return numpy.matmul(original,trans_matrix)
 
 def translate_func():
+    global lines_array
+    global transforms 
+    x_trans = 0
+    y_trans = 0
+    # checks for lengtsh
+    if len(translate_x_entry.get()) != 0:
+        x_trans = translate_x_entry.get()
+    if len(translate_y_entry.get()) != 0:
+        y_trans = translate_y_entry.get()
+
+    # multiply and add transforms to transform matrix
+    trans_matrix = numpy.array([[1, 0, 0],[0, 1, 0],[translate_x_entry.get(),translate_y_entry.get(), 1]])
+    trans_matrix = trans_matrix.astype(float)
+    
+    # print(transforms)
+    # print(trans_matrix)
+    # transforms and updates user
+    transforms = basic_translate(transforms,translate_x_entry.get(),translate_y_entry.get())
+    transform_text_box.delete('1.0','end')
+    transform_text_box.insert(END,transforms)
     print('translate')
 
 def basic_scale_func():
@@ -41,23 +73,38 @@ def rotate_func():
 
 def display_image():
     print('display')
-    
+
+def insert_line():
+    print('insert Line')
 if __name__ == '__main__':
+    
+
     app = Tk()
     app.title('Transmongus')
     app.geometry('1000x800')
     
     # add data files inputs
-    input_text = StringVar()
-    input_label = Label(app,text='Input File', font=('bold', 12), pady=20)
-    input_label.grid(row=0,column=0,sticky=W)
 
+    input_x0 = StringVar()
+    input_x1 = StringVar()
+    input_y0 = StringVar()
+    input_y1 = StringVar()
+    
+    input_label = Label(app, text='Lines')
+    input_label.grid(row=9,column=0)
+    input_x0_box = Entry(app, textvariable=input_x0)
+    input_x0_box.grid(row=9,column=1, sticky=W)
+    input_y0_box = Entry(app, textvariable=input_y0)
+    input_y0_box.grid(row=9,column=2, sticky=W)
+    input_x1_box = Entry(app, textvariable=input_x1)
+    input_x1_box.grid(row=9,column=3,sticky=W)
+    input_y1_box = Entry(app, textvariable=input_y1)
+    input_y1_box.grid(row=9,column=4,sticky=W)
+    
     # output file
     output_text = StringVar()
-    output_label = Label(app,text='Output File', font=('bold', 12), pady=20)
-    output_label.grid(row=1,column=0,sticky=W)
     output_entry = Entry(app, textvariable=output_text)
-    output_entry.grid(row=1, column=1)
+    output_entry.grid(row=0, column=2)
     
     # translate X and Y
     x_text = StringVar()
@@ -104,20 +151,25 @@ if __name__ == '__main__':
     rotate_cy_entry.grid(row=6, column=2)
     
     # displays data points
-    text_box = Text(app, width=25, height=20)
-    text_box.grid(row=10,column=1)
+    text_box_label = Label(app, text='Points and Transforms')
+    text_box_label.grid(row=11,column=0)
+    text_box = Text(app, width=20, height=15)
+    text_box.grid(row=11,column=1)
 
     text_box_info = Text(app,width=10,height=1)
-    text_box_info.grid(row=10,column=3)
+    text_box_info.grid(row=11,column=3)
 
+    transform_text_box = Text(app,width=20,height=15)
+    transform_text_box.grid(row=11,column=2)
+    
     # Buttons
     # import button
     import_button = Button(app,text='Import File',command=import_file)
-    import_button.grid(row=0,column=1)
+    import_button.grid(row=0,column=0)
 
     # export button
     export_button = Button(app, text='Export File',command=export_file)
-    export_button.grid(row=1, column=2)
+    export_button.grid(row=0, column=3)
 
     # translate button
     translate_button = Button(app, text='Translate',command=translate_func)
@@ -139,8 +191,13 @@ if __name__ == '__main__':
 
     # Display Image
     display_button = Button(app,text='Display', command=display_image)
-    display_button.grid(row=10,column=2)
+    display_button.grid(row=11,column=4)
 
-    app.mainloop()    
+    # insert 
+    insert_button = Button(app,text='Insert Line',command=insert_line)
+    insert_button.grid(row=9,column=5)
+
+    app.mainloop()
+    
 
     
